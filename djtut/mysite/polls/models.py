8956c7a5732bb3c2097bfdd3c62ea84873668f3b
James McKernan <james@spider-man.corp.slide.com>
2010-05-19 04:38:01 -0700

    putting tutorial on django 1.2

--- djtut/mysite/polls/models.py
+++ djtut/mysite/polls/models.py
@@ -4,14 +4,14 @@ import datetime
 
 class Poll(models.Model):
 	question = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('dat published')
+	pub_date = models.DateTimeField('date published')
 	
 	def __unicode__(self):
 		return self.question
-	
+
 	def was_published_today(self):
-		return self.pub_date.date() == datetime.date.today()
-		
+		return self.pub_date == datetime.date.today()
+
 class Choice(models.Model):
 	poll   = models.ForeignKey(Poll)
 	choice = models.CharField(max_length=200)
@@ -19,5 +19,3 @@ class Choice(models.Model):
 	
 	def __unicode__(self):
 		return self.choice
-		
-		
